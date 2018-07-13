cb660208e9721e505c05851420bc882b8f0b2ae3
junghyun <adanam872@gmail.com>
2018-07-13 10:06:45 +0900

    copy from web-09

---
+++ bitcamp-web-10/src/main/java/bitcamp/pms/controller/MemberUpdateController.java
@@ -0,0 +1,59 @@
+package bitcamp.pms.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bitcamp.pms.annotation.Autowired;
+import bitcamp.pms.annotation.Controller;
+import bitcamp.pms.annotation.RequestMapping;
+import bitcamp.pms.dao.MemberDao;
+import bitcamp.pms.domain.Member;
+
+@Controller("/member/update")
+public class MemberUpdateController {
+
+    MemberDao memberDao;
+
+    public MemberUpdateController() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public MemberUpdateController(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Autowired
+    public void setMemberDao(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @RequestMapping
+    public String update(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        request.setCharacterEncoding("UTF-8");
+
+        Member member = new Member();
+        member.setId(request.getParameter("id"));
+        member.setEmail(request.getParameter("email"));
+        member.setPassword(request.getParameter("password"));
+
+        int count = memberDao.update(member);
+        if (count == 0) {
+            return "/member/updatefail.jsp";
+        } else {
+            return "redirect:list";
+        }
+    }
+
+}
